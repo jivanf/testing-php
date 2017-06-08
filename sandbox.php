c5bf3133791573bafe69c9486a9e1a60c622698a
Jivan <minecraftjivan@gmail.com>
2017-06-07 20:12:50 -0700

    using variables and displaying them

---
+++ sandbox.php
@@ -0,0 +1,7 @@
+<?php
+$number = 14;
+$color = "red";
+$adjective = "fat";
+echo "<strong> $color </strong> is a color<br>";
+echo "<strong> $number </strong> is a number<br>";
+echo "<strong> $adjective </strong> is an adjective<br>";

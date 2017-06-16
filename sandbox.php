2ff355449390edc8e011bfdd3ed8b874cf198169
Jivan <minecraftjivan@gmail.com>
2017-06-15 20:06:34 -0700

    using the null-coalesce operator

--- sandbox.php
+++ sandbox.php
@@ -1,45 +1,4 @@
 <?php
-$book = [
-    'title' => "The Hitchhiker's Guide to the Galaxy",
-    'author' => 'Douglas Adams',
-    'description' => 'a comedy sci-fi adventure originally based on a BBC radio series'
-];
-
-
-$characters = [
-    'Arthur Dent',
-    'Ford Prefect',
-    'Zaphod Beeblebrox',
-    'Marvin, the paranoid android',
-    'Slartibartfast'
-];
-
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="styles.css"/>  
-    <meta charset="UTF-8">
-    <title>Embedding PHP in HTML</title>
-</head>
-<body>
-<h1><?= $book['title']; ?> by <?=$book['author']?></h1>
-<p><?= $book['title']; ?> is <?= $book['description']?></p>
-<p></p>
-
-<h2>Main Characters</h2>
-<ul>
-    <?php
-    for ($i = 0; $i < sizeof($characters); $i++) {
-        echo "<li>";
-        echo $characters[$i];
-        echo "</li>";
-
-    }
-    ?>
-</ul>
-
-
-</body>
-</html>
+$number = 25;
+$secondNumber = 50;
+echo $secondNumber ?? $number ?? "Can't find number variable.";
